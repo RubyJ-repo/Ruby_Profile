c0bcbbf7a8bde8f465d30ab36d575f7536a1c89c
Timothy Yang <u7494610@anu.edu.au>
2023-06-13 04:41:59 +1000

    bug fixes

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const favicon = document.getElementById('favicon')
   const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const attentionMessage = 'Pls Come back'
 
   document.addEventListener('visibilitychange', function (e) {
     const isPageActive = !document.hidden
@@ -11,7 +11,7 @@ window.onload = function () {
   function toggle(isPageActive) {
     if (isPageActive) {
       document.title = pageTitle
-      favicon.href = './assets/images/dp_male.svg'
+      favicon.href = './assets/images/John-Doe.png'
     } else {
       document.title = attentionMessage
       favicon.href = './assets/images/folded.png'
